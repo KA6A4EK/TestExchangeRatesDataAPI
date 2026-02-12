80ef5a29e417de3e26b43ada5b709cf742b4cbc1
vadim <yannurov20032@gmail.com>
2026-02-12 23:03:11 +0500

    Enhance BaseCurrencyDropdown with custom dropdown icons for expanded/collapsed states

--- app/src/main/java/com/example/testexchangeratesdataapi/presentation/currencies/components/BaseCurrencyDropdown.kt
+++ app/src/main/java/com/example/testexchangeratesdataapi/presentation/currencies/components/BaseCurrencyDropdown.kt
@@ -6,10 +6,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,8 +22,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.testexchangeratesdataapi.R
 import com.example.testexchangeratesdataapi.presentation.currencies.components.state.CurrenciesUiState
+import com.example.testexchangeratesdataapi.ui.theme.BlueMain
 import com.example.testexchangeratesdataapi.ui.theme.PrimaryLight
 import com.example.testexchangeratesdataapi.ui.theme.White
 
@@ -58,7 +63,9 @@ fun BaseCurrencyDropdown(
                 text = selected,
                 style = MaterialTheme.typography.titleMedium,
             )
-            ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+            Icon(
+                painterResource(if (!expanded) R.drawable.dropdown else R.drawable.dropup), contentDescription = "", tint = BlueMain
+            )
         }
 
         ExposedDropdownMenu(
